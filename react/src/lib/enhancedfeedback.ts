6a2edde8362d7991f5bdeccdb2c7ea80ae57b9b0
radkinz <radkins22@students.stab.org>
2023-05-30 02:50:34 -0400

    add chatgpt api'

--- react/src/lib/enhancedfeedback.ts
+++ react/src/lib/enhancedfeedback.ts
@@ -8,7 +8,8 @@ export enum FeedbackAnimation {
 }
 
 export type FeedbackResponse = {
-  text: string
+  ai_text: string
+  text: string[]
   animation: FeedbackAnimation
 }
 
@@ -16,16 +17,17 @@ export type EmotionalFeedback = {
   onReallyClose: FeedbackResponse
   onFastGuess: FeedbackResponse
   onSlowGuess: FeedbackResponse
-  firstGuess: FeedbackResponse[]
-  fifthGuess: FeedbackResponse[]
-  sixthGuess: FeedbackResponse[]
-  onGettingClose: FeedbackResponse[]
-  newHighScore: FeedbackResponse[]
-  notNewHighScore: FeedbackResponse[]
+  firstGuess: FeedbackResponse
+  fifthGuess: FeedbackResponse
+  sixthGuess: FeedbackResponse
+  newGame: FeedbackResponse
+  onGettingClose: FeedbackResponse
+  newHighScore: FeedbackResponse
+  notNewHighScore: FeedbackResponse
   invalidWord: FeedbackResponse
-  onWin: FeedbackResponse[]
+  onWin: FeedbackResponse
   onLoss: FeedbackResponse
-  onIdle: FeedbackResponse[]
+  onIdle: FeedbackResponse
 }
 
 export const getFeedbackKey = (feedbackResponse: FeedbackResponse): string | null => {
@@ -48,64 +50,36 @@ export const getFeedbackKey = (feedbackResponse: FeedbackResponse): string | nul
 }
 
 const HELPFUL_TEDDY = {
-  onReallyClose: { text: 'You’re so, so close. You got this!', animation: FeedbackAnimation.WaveShort },
-  onFastGuess: { text: 'Wow, you\'re so fast! Incredible!', animation: FeedbackAnimation.WaveShort },
-  onSlowGuess: { text: 'Taking your time really paid off!', animation: FeedbackAnimation.WaveShort },
-  firstGuess: [
-    { text: 'Good luck! You got this!', animation: FeedbackAnimation.WaveShort },
-    { text: 'Another round! You can do this!', animation: FeedbackAnimation.WaveShort },
-    { text: 'You\'ve got the hang of this!', animation: FeedbackAnimation.WaveShort },
-    { text: 'I know you can get this one!', animation: FeedbackAnimation.WaveShort }
-  ],
-  fifthGuess: [
-    { text: 'Two guesses left, that\'s plenty of time!', animation: FeedbackAnimation.Idle },
-    { text: 'Last two guesses! Trust yourself, you got this.', animation: FeedbackAnimation.Idle },
-    { text: 'This is a tough one, but you\'re close!', animation: FeedbackAnimation.Idle },
-    { text: 'This one can be hard, but I believe in you!', animation: FeedbackAnimation.Idle }
-  ],
-  sixthGuess: [
-    { text: 'Just breathe and think it through. You got this!', animation: FeedbackAnimation.Wave },
-    { text: 'Stay calm and use all the facts you uncovered.', animation: FeedbackAnimation.Wave },
-    { text: 'Your final chance. You can do it!', animation: FeedbackAnimation.Wave },
-    { text: 'Don\'t give up now! Stay calm and breathe.', animation: FeedbackAnimation.Wave }
-  ],
-  onGettingClose: [
-    { text: 'You\'re getting closer!', animation: FeedbackAnimation.WaveShort },
-    { text: 'Oh nice, that really narrowed the field!', animation: FeedbackAnimation.WaveShort },
-    { text: 'Ooh, you\'re getting close now!', animation: FeedbackAnimation.WaveShort },
-    { text: 'That was a really good guess!', animation: FeedbackAnimation.WaveShort }
-  ],
-  newHighScore: [
-    { text: 'Wow! What a great guess!', animation: FeedbackAnimation.Success },
-    { text: 'Ooh nice one! I didn\'t think of that.', animation: FeedbackAnimation.Success },
-    { text: 'You learned more information! Nice work!', animation: FeedbackAnimation.Success },
-    { text: 'Great guess!', animation: FeedbackAnimation.Success }
-  ],
-  notNewHighScore: [
-    { text: 'Okay! Well now we know what doesn\'t work.', animation: FeedbackAnimation.SlightlyHappy },
-    { text: 'Nice! now we know what to avoid.', animation: FeedbackAnimation.SlightlyHappy },
-    { text: 'Aww, I was sure that would be it.', animation: FeedbackAnimation.Sadness },
-    { text: 'Hmm, what could it be?!', animation: FeedbackAnimation.Sadness }
-  ],
+  onReallyClose: { ai_text: 'Say a new 20 character empathetic message to someone who is close to geussing the right word', text: ["You’re so, so close. You got this!"],animation: FeedbackAnimation.WaveShort },
+  onFastGuess: { ai_text: 'Say a new 20 character empathetic message to someone who guessed a word in wordle really fast', text: ['Wow, you\'re so fast! Incredible!'], animation: FeedbackAnimation.WaveShort },
+  onSlowGuess: { ai_text: 'Say a new 20 character empathetic message to someone who took a long time to guess a word', text: ['Taking your time really paid off!'], animation: FeedbackAnimation.WaveShort },
+  firstGuess: { ai_text: 'Say a new 20 character empathetic message to someone after their first wordle attempt',text: ['Good luck! You got this!','Another round! You can do this!','You\'ve got the hang of this!', 'I know you can get this one!'], animation: FeedbackAnimation.WaveShort },
+  fifthGuess: 
+    { ai_text: 'Say a new 20 character empathetic message to someone on their second to last wordle attempt', text: ['Two guesses left, that\'s plenty of time!', 'Last two guesses! Trust yourself, you got this.','This is a tough one, but you\'re close!','This one can be hard, but I believe in you!'], animation: FeedbackAnimation.Idle },
+  sixthGuess: 
+    { ai_text: 'Say a new 20 character empathetic message to someone on their last wordle attempt', text: ['Just breathe and think it through. You got this!','Stay calm and use all the facts you uncovered.','Your final chance. You can do it!','Don\'t give up now! Stay calm and breathe.'], animation: FeedbackAnimation.Wave }
+  ,
+  newGame: 
+    { ai_text: 'Say a new 20 character empathetic message to someone starting a new wordle game and is on their first geussS',text: ["You got this!"],animation: FeedbackAnimation.WaveShort }
+  ,
+  onGettingClose: 
+    { ai_text: 'Say a new 20 character empathetic message to someone whose close to geussing the right word',text: ['You\'re getting closer!','Oh nice, that really narrowed the field!','Ooh, you\'re getting close now!','That was a really good guess!'],animation: FeedbackAnimation.WaveShort }
+  ,
+  newHighScore: 
+    { ai_text: 'Say a new 20 character empathetic message to someone who just set a new high score',text: ['Wow! What a great guess!','Ooh nice one! I didn\'t think of that.', 'You learned more information! Nice work!','Great guess!'],animation: FeedbackAnimation.Success }
+  ,
+  notNewHighScore: 
+    { ai_text: 'Okay! Well now we know what doesn\'t work.',text: ['Okay! Well now we know what doesn\'t work.','Nice! now we know what to avoid.','Aww, I was sure that would be it.', 'Hmm, what could it be?!'],animation: FeedbackAnimation.SlightlyHappy }
+  ,
   invalidWord: {
-    text: 'Oops! I don\'t know that word! Give it another try.',
+    ai_text: 'Oops! I don\'t know that word! Give it another try.', text: ['Oops! I don\'t know that word! Give it another try.'],
     animation: FeedbackAnimation.Sadness
   },
-  onWin: [
-    { text: 'This must be your lucky day!!', animation: FeedbackAnimation.Success },
-    { text: 'Two guesses?! Are you a wizard?!', animation: FeedbackAnimation.Success },
-    { text: 'Three guesses? You\'re a rock star!', animation: FeedbackAnimation.Success },
-    { text: 'Great job! You won in four guesses!', animation: FeedbackAnimation.Success },
-    { text: 'You did it! You won in five guesses!', animation: FeedbackAnimation.Success },
-    { text: 'That was close, but you did it!', animation: FeedbackAnimation.Success }
-  ],
-  onLoss: { text: 'You almost had it! Let\'s try again.', animation: FeedbackAnimation.Sadness },
-  onIdle: [
-    { text: 'It\'s good to think it through carefully.', animation: FeedbackAnimation.WaveShort },
-    { text: 'I believe in you!', animation: FeedbackAnimation.WaveShort },
-    { text: 'It\'s okay to feel stumped. You\'ll get it!', animation: FeedbackAnimation.WaveShort },
-    { text: 'This one is a toughy, isn\'t it?', animation: FeedbackAnimation.Sadness }
-  ]
+  onWin: { ai_text: 'Say a new 20 character empathetic message to someone who just won wordle',text: ['You did it!'],animation: FeedbackAnimation.Success }
+  ,
+  onLoss: { ai_text: 'You almost had it! Let\'s try again.',text: ["You almost had it! Let\'s try again."],animation: FeedbackAnimation.Sadness },
+  onIdle: { ai_text: 'Say a new 20 character empathetic message to someone who is taking a long time to guess a word in wordle',text: ["It\'s good to think it through carefully."],animation: FeedbackAnimation.WaveShort }
+  
 }
 
 export const getEmotionalFeedback = (): EmotionalFeedback => {
